30171a2538bfaa1695f90371a86a362e9ee87676
Kolesnikov Nikolay Nikolaevich <nikolesnikov@ozon.ru>
2024-09-25 23:48:37 +0300

    impl

---
+++ sharding-repl-cache/scripts/mongo-init.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+echo "Инициализируем config mongo-сервер"
+docker compose exec -T config_server mongosh --port 27017 --quiet <<EOF
+print("running rs.initiate...");
+rs.initiate(
+  {
+    _id: "config_server",
+       configsvr: true,
+    members: [
+      {_id: 0, host: "config_server:27017"}
+    ]
+  }
+);
+EOF
+
+sleep 1
+
+echo "Инициализируем первый шард mongo"
+docker compose exec -T shard1 mongosh --port 27018 --quiet <<EOF
+print("running rs.initiate...");
+rs.initiate(
+    {
+      _id: "shard1",
+      members: [
+        {_id: 0, host: "shard1:27018"},
+        {_id: 1, host: "shard1_1:27021"}
+      ]
+    }
+);
+EOF
+
+sleep 1
+
+echo "Инициализируем второй шард mongo"
+docker compose exec -T shard2 mongosh --port 27019 --quiet <<EOF
+print("running rs.initiate...");
+rs.initiate(
+    {
+      _id: "shard2",
+      members: [
+        {_id :0, host: "shard2:27019"},
+        {_id :1, host: "shard2_1:27022"}
+      ]
+    }
+);
+EOF
+
+sleep 1
+
+echo "Настраиваем работу роутера и предзаполняем мок-данные"
+docker compose exec -T mongos_router mongosh --port 27020 --quiet <<EOF
+print("running sh.addShard...");
+sh.addShard( "shard1/shard1:27018");
+
+print("running sh.addShard...");
+sh.addShard( "shard2/shard2:27019");
+
+print("running sh.enableSharding...");
+sh.enableSharding("somedb");
+
+print("running sh.shardCollection...");
+sh.shardCollection("somedb.helloDoc", {"name": "hashed"} )
+
+use somedb
+
+print("running inserts mock data...");
+for(var i = 0; i < 1000; i++) db.helloDoc.insert({age:i, name:"ly"+i})
+EOF
+
+echo "Кол-во элементов в primary шардах"
+docker compose exec -T shard1 mongosh --port 27018 --quiet <<EOF
+use somedb;
+const count = db.helloDoc.countDocuments();
+print("shard1",count)
+EOF
+docker compose exec -T shard2 mongosh --port 27019 --quiet <<EOF
+use somedb;
+const count = db.helloDoc.countDocuments();
+print("shard2",count)
+EOF
+
+echo "Кол-во элементов в репликах шардов"
+docker compose exec -T shard1_1 mongosh --port 27021 --quiet <<EOF
+use somedb;
+const count = db.helloDoc.countDocuments();
+print("replica shard1",count)
+EOF
+docker compose exec -T shard2_1 mongosh --port 27022 --quiet <<EOF
+use somedb;
+const count = db.helloDoc.countDocuments();
+print("replica shard2",count)
+EOF
+
+echo "Команды выполнены перейдите на http://localhost:8080 и http://localhost:8080/docs для финальной проверки"
+
+echo "Добавляем и считываем в redis_master тестовое значение"
+docker compose exec -T redis_master redis-cli <<EOF
+set testkey testvalue
+get testkey
+EOF
+
+sleep 1
+
+echo "Считываем в redis_slave тестовое значение"
+docker compose exec -T redis_slave redis-cli <<EOF
+get testkey
+EOF
+
+echo "Проверяем скорость запросов с redis"
+curl  -kv -w '\n* Response time: %{time_total}s\n' \
+  -X 'GET' \
+  'http://localhost:8080/helloDoc/users/username2' \
+  -H 'accept: application/json'
+
+curl  -kv -w '\n* Response time: %{time_total}s\n' \
+  -X 'GET' \
+  'http://localhost:8080/helloDoc/users/username2' \
+  -H 'accept: application/json'
